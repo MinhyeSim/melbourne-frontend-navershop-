5362807a820f6474962950845e28d1828fa8e120
MinhyeSim <smine0032@gmail.com>
2022-02-24 16:54:13 +0900

    2022-02-24 16:54

--- components/Bmi.js
+++ components/Bmi.js
@@ -1,11 +1,25 @@
 import React, { useState } from "react"
 import Layout from "../containers/Layout"
-
+import axios from "axios";
 export default function Bmi (){
     const[username, setUsername] = useState("");
     const[height, setHeight] = useState(0.0);
     const[weight, setWeight] = useState(0.0);
-    
+    const getBmi = (e) => { 
+    e.preventDefault()
+    alert('버튼클릭')
+        try {
+             axios.get('http://localhost:8080/member/bmi/aaa/180.5/80.5')
+             .then((res)=>{
+                 alert(res + `답장이 도착했습니다 [내용] ${JSON.stringify(res.data)}`)
+
+             })
+        }catch(error){
+            alert(`에러가 발생했습니다 [내용] ${error}`)
+
+        }
+    }
+
     const result = () => {
         let username = document.getElementById("username").value
          console.log('name :'+username)
@@ -19,24 +33,19 @@ export default function Bmi (){
     }
 
     
-    return (<Layout><h1>Bmi폼</h1>
-
-        
+    return (<Layout>
+    <h1>Bmi폼</h1>
     <div>
     <label><b>Username</b></label>
-    <input id = "username" type=""/><br/>
+    <input id = "username"/><br/>
 
-    <label htmlFor=""><b>height</b></label>
-    <input id = "height" type=""/><br/>
+    <label><b>height</b></label>
+    <input id = "height"/><br/>
 
-    <label htmlFor=""><b>weight</b> </label>
-    <input id = "weight" type=""/><br/>
-
-    <button onClick={()=>{result()}}>결정</button>
-   
-    <div>
-        이름:{username} 키:{height} 몸무게 :{weight} 
-    </div>
+    <label><b>weight</b> </label>
+    <input id = "weight"/><br/>
+    <input type="button" onClick={getBmi} value = "BMI 체크" /> <br/>
+      
     </div>
 
     </Layout>)
